aa3f5d63f631feacd0ace725a5a74fc4fe8ae66e
Niels Erik Ostergaard <neo@neoGame.dk>
2014-08-04 12:07:54 +0200

    Added a hello_denmark() method

--- src/HelloWorld.cxx
+++ src/HelloWorld.cxx
@@ -2,13 +2,20 @@
 
 class HelloWorld {
 public:
-  void hello(void) {
+
+  void hello_world(void) {
     std::cout << "Hello world. Nice to meet you." << std::endl;
   }
+
+  void hello_denmark(void) {
+    std::cout << "Hello Denmark." << std::endl;
+  }
+
 };
 
 int main(int argc, char* argv[]) {
   HelloWorld world;
 
-  world.hello();
+  world.hello_world();
+  world.hello_denmark();
 }
